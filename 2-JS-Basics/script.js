cb310d50ea483717f3383bde29ef4d1f21cdb3c8
bofur24 <dransiktolkien@yahoo.com>
2020-07-08 13:50:37 -0500

    Variables

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,23 @@
-console.log('Hello World!!!');
+//JavaScript has dynamic typing: 
+//Data types are automatically assigned to variables
+
+var firstName = 'John'; //Type String
+console.log(firstName);
+
+var lastName = 'Smith'; // Type String
+var age = 28; // Type number
+
+var fullAge = true; // Type Blooean
+console.log(fullAge);
+
+var job; // Type Undefined 
+console.log(job);
+
+job = 'Teacher'
+console.log(job);
+
+// Rule variables must start with _ $ or letter only.
+var _3years = 3;
+var johnMark = 'John and Mark';
+
+// no JavaScript keywords can be used as variables
