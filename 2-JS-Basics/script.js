c965c66bce2c0e1c55bebdcce23479b16c301df1
bofur24 <dransiktolkien@yahoo.com>
2020-07-09 07:07:30 -0500

    Commit Change

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -249,19 +249,19 @@ console.log(x);
 // falsy values: undefined, null, 0, '', NaN
 // truthy values: NOT falsy values
 
-var height;
+// var height;
 
-height = 23;
+// height = 23;
 
-if (height || height === 0) {
-  console.log('Variable is defined')
-} else {
-  console.log('Variable has NOT been defined');
-}
+// if (height || height === 0) {
+//   console.log('Variable is defined')
+// } else {
+//   console.log('Variable has NOT been defined');
+// }
 
-// Equality Operators
+// // Equality Operators
 
-if(height == 23) {
-  console.log('The == operator does type coercion!');
-}
+// if(height == 23) {
+//   console.log('The == operator does type coercion!');
+// }
 
